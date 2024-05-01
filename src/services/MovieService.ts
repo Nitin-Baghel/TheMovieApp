a51c644903ad9ea35ab2f244c763cca32c4f4741
Nitin Baghel <baghelnitin95@gmail.com>
2024-05-01 11:17:29 +0530

    optimised code

--- src/services/MovieService.ts
+++ src/services/MovieService.ts
@@ -1,22 +1,4 @@
-// src/services/BookService.ts
 import axios from 'axios';
-
-const BASE_URL = 'https://www.googleapis.com/books/v1/volumes';
-
-export const getBooks = async () => {
-  try {
-    const response = await axios.get(`${BASE_URL}?q=javascript`);
-    return response.data.items.map((item: any) => ({
-      title: item.volumeInfo.title,
-      author: item.volumeInfo.authors
-        ? item.volumeInfo.authors.join(', ')
-        : 'Unknown Author',
-    }));
-  } catch (error) {
-    console.error('Error fetching books:', error);
-    throw error;
-  }
-};
 
 export const getMovies = async () => {
   try {
